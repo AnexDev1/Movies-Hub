83455488ae08a009d59f9cef9451b1a3cb3064fb
anexdev1 <anwarnasir0970@gmail.com>
2023-08-23 03:05:44 -0400

    tested some new components to add

--- src/starRatings.js
+++ src/starRatings.js
@@ -17,6 +17,7 @@ const textStyle = {
 
 export default function StarRating({ maxRating = 5 }) {
   const [rating, setRating] = useState(0);
+  const [tempRating, setTempRating] = useState(0);
 
   function handleRating(rate) {
     setRating(rate);
@@ -28,11 +29,13 @@ export default function StarRating({ maxRating = 5 }) {
           <Star
             key={i}
             onRate={() => handleRating(i + 1)}
-            full={rating >= i + 1}
+            onHoverIn={() => setTempRating(i + 1)}
+            onHoverOut={() => setTempRating(0)}
+            full={tempRating ? tempRating >= i + 1 : rating >= i + 1}
           />
         ))}
       </div>
-      <p style={textStyle}>{rating || ""}</p>
+      <p style={textStyle}>{tempRating || rating || ""}</p>
     </div>
   );
 }
@@ -42,9 +45,15 @@ const starStyle = {
   display: "block",
   cursor: "pointer",
 };
-function Star({ onRate, full }) {
+function Star({ onRate, full, onHoverIn, onHoverOut }) {
   return (
-    <span role="button" onClick={onRate} style={starStyle}>
+    <span
+      role="button"
+      onClick={onRate}
+      onMouseEnter={onHoverIn}
+      onMouseLeave={onHoverOut}
+      style={starStyle}
+    >
       {full ? (
         <svg
           xmlns="http://www.w3.org/2000/svg"
