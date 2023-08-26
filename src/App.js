38006a300fbde808487327fdd19a68e491f741eb
anexdev1 <anwarnasir0970@gmail.com>
2023-08-26 13:48:17 +0300

    fully implemented movie details screen

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import StarRating from "./starRatings";
 
 // const tempMovieData = [
 //   {
@@ -211,11 +212,60 @@ function Movie({ movie, onSelectMovie }) {
 }
 
 function MovieDetails({ selectedId, onCloseMovie }) {
+  const [movie, setMovie] = useState({});
+  const {
+    Actors: actors,
+    Director: director,
+    Genre: genre,
+    Plot: plot,
+    Released: released,
+    Runtime: runtime,
+    Title: title,
+    Year: year,
+    imdbRating,
+    Poster: poster,
+  } = movie;
+  useEffect(
+    function () {
+      async function GetMovieDetails() {
+        const res = await fetch(
+          `http://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
+        );
+        const data = await res.json();
+        setMovie(data);
+      }
+      GetMovieDetails();
+    },
+    [selectedId]
+  );
   return (
-    <div>
-      <button className="btn-back" onClick={onCloseMovie}>
-        &larr;
-      </button>
+    <div className="details">
+      <header>
+        <button className="btn-back" onClick={onCloseMovie}>
+          &larr;
+        </button>
+        <img src={poster} alt={`poster of ${title} movie`} />
+        <div className="details-overview">
+          <h2>{title}</h2>
+          <p>
+            {released} &bull; {runtime}
+          </p>
+          <p>{genre}</p>
+          <p>
+            <span>⭐</span> {imdbRating} IMDb Rating
+          </p>
+        </div>
+      </header>
+      <section>
+        <div className="rating">
+          <StarRating maxRating={10} size={24} />
+        </div>
+        <p>
+          <em>{plot}</em>
+        </p>
+        <p>Starring {actors}</p>
+        <p>Directed by {director}</p>
+      </section>
     </div>
   );
 }
