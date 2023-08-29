2f324ad202c46035860b19f7a928d7d46a6797cd
anexdev1 <anwarnasir0970@gmail.com>
2023-08-29 21:27:27 +0300

    implemented more error handling and cleanup

--- src/App.js
+++ src/App.js
@@ -34,20 +34,26 @@ export default function App() {
   }
   useEffect(
     function () {
+      const controller = new AbortController();
+
       async function fetchMovies() {
         try {
           setIsLoading(true);
           setError("");
           const res = await fetch(
-            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`
+            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
+            { signal: controller.signal }
           );
           if (!res.ok) throw new Error("Something went wrong!");
           const data = await res.json();
           if (data.Response === "False") throw new Error("Movie not Found");
           setMovies(data.Search);
+          setError("");
         } catch (err) {
           console.error(err);
-          setError(err.message);
+          if (err.name !== "AbortError") {
+            setError(err.message);
+          }
         } finally {
           setIsLoading(false);
         }
@@ -57,7 +63,12 @@ export default function App() {
         setError("");
         return;
       }
+      handleCloseDetail();
       fetchMovies();
+
+      return function () {
+        controller.abort();
+      };
     },
     [query]
   );
